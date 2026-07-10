12b663a3f12601c37c4efd528b067a603e58945c
Wilfredpin <Wilfredlovez13@gmail.com>
2026-07-10 06:12:48 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'; // Import React and ReactDOM from the 'react' and 'react-dom/client' packages.
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootEl = document.getElementById('root'); // Get the root element from the HTML document.
+const root = ReactDOM.createRoot(rootEl); // Create a root.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<h1>hello World</h1>); // Render a simple "hello World" message to the root element.
+
+
