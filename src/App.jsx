0d47a3061a965535ef568b846761c0cb34af3013
Wilfredpin <Wilfredlovez13@gmail.com>
2026-07-13 05:24:45 +0100

    Update take 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import { useState } from 'react'
+import Header from './Header.jsx'
 
 function App() {
 
+  return (
+    <Header></Header>
+  );
 }
-export default App
+
+export default App;
+
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,8 +2,17 @@
 function Header() {
     return (
         <header>
-            <h1>My React App</h1>
+            <nav>
+                <h1>My React App</h1>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
         </header>
+    
     );
 }
 
